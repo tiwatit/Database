78baaaff6b10f57151487cbd93e3ce03c5fab9ea
tiwatit <72233168+tiwatit@users.noreply.github.com>
2020-12-10 06:20:47 +0200

    Add files via upload

---
+++ DB_Lab2/DB_Lab2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using lab2.MVC;
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller m = new Controller();
+            while (1 == 1)
+            {
+                if (m.entity_menu() == 1)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
